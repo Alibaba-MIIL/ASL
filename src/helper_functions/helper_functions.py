2bac34012c4c950a60d9f1a95d6ba8a10a7dfc43
talrid <tal.ridnik@alibaba-inc.com>
2020-11-11 10:26:38 +0200

    added nus_wide model

--- src/helper_functions/helper_functions.py
+++ src/helper_functions/helper_functions.py
@@ -8,18 +8,14 @@ import torch
 def parse_args(parser):
     # parsing args
     args = parser.parse_args()
-    if args.dataset_type == 'MS-COCO':
-        args.do_bottleneck_head = False
-        if args.th == None:
-            args.th = 0.7
-    elif args.dataset_type == 'PASCAL-VOC':
-        args.do_bottleneck_head = False
-        if args.th == None:
-            args.th = 0.7
-    elif args.dataset_type == 'OpenImages':
+    if args.dataset_type == 'OpenImages':
         args.do_bottleneck_head = True
         if args.th == None:
             args.th = 0.995
+    else:
+        args.do_bottleneck_head = False
+        if args.th == None:
+            args.th = 0.7
     return args
 
 
