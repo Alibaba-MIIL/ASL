0a21a18a400934e94fd099fbeeed187bd10433f5
talrid <tal.ridnik@alibaba-inc.com>
2021-03-23 19:11:45 +0200

    True

--- src/loss_functions/losses.py
+++ src/loss_functions/losses.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class AsymmetricLoss(nn.Module):
-    def __init__(self, gamma_neg=4, gamma_pos=1, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=False):
+    def __init__(self, gamma_neg=4, gamma_pos=1, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=True):
         super(AsymmetricLoss, self).__init__()
 
         self.gamma_neg = gamma_neg
