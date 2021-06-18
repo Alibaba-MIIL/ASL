51a2f94dbf078cded67111eb0975dbd4228c2907
Erwin <e.l.vanthiel@student.nl>
2021-06-18 14:34:00 +0200

    perform the attack on imagenet examples

--- attack.py
+++ attack.py
@@ -12,6 +12,7 @@ from src.models import create_model
 from src.loss_functions.losses import AsymmetricLoss
 from randaugment import RandAugment
 from torch.cuda.amp import GradScaler, autocast
+from pgd import create_targeted_adversarial_examples
 
 parser = argparse.ArgumentParser(description='PyTorch MS_COCO Training')
 parser.add_argument('data', metavar='DIR', help='path to dataset', default='/home/MSCOCO_2014/')
@@ -39,41 +40,16 @@ TARGET_INDEX = 60 # label for donut object
 
 # Configure mlc model
 model = create_model(args).cuda()
-model_state = torch.load("mlc-model-epoch100", map_location=device)
+model_state = torch.load("mlc-model-epoch50", map_location=device)
 model.load_state_dict(model_state["state_dict"])
 model.eval()
 model.to(device)
-
-# Generate adversarials with targeted PGD attack
-# source: https://github.com/Harry24k/PGD-pytorch/blob/master/PGD.ipynb
-def create_adversarial_examples(model, images, target_class, eps=0.6, alpha=2/255, iters=40, device='cpu'):
-    images = images.to(device)
-    target_class = target_class.to(device).float()
-    model = model.to(device)
-    # loss = nn.CrossEntropyLoss()
-    loss = nn.BCELoss()
-
-    ori_images = images.data
-        
-    for i in range(iters):    
-        images.requires_grad = True
-        outputs = sigmoid(model(images)).to(device)
-
-        model.zero_grad()
-        cost = loss(outputs, target_class).to(device)
-        cost.backward()
-
-        adv_images = images - alpha*images.grad.sign()
-        eta = torch.clamp(adv_images - ori_images, min=-eps, max=eps)
-        images = torch.clamp(ori_images + eta, min=0, max=1).detach_()
-            
-    return images
 
 
 # Load the data
 instances_path_train = os.path.join(args.data, 'annotations/instances_train2014.json')
 # data_path_train = args.data
-data_path_train = f'{args.data}/train2014'  # args.data
+data_path_train = '{0}/train2014'.format(args.data)
 train_dataset = CocoDetection(data_path_train,
                               instances_path_train,
                               transforms.Compose([
@@ -99,15 +75,15 @@ pred = (sigmoid(model(images)) > 0.5).int().to(device)
 # Perform PGD attack and repeat
 target = torch.clone(pred)
 target[:, TARGET_INDEX] = 1
-adversarials = create_adversarial_examples(model, images, target, device=device)
+adversarials = create_targeted_adversarial_examples(model, images, target, device=device)
 pred_after_attack = (sigmoid(model(adversarials)) > 0.5).int()
 
 # target_tensor = torch.zeros(80).int()
 # target_tensor[TARGET_INDEX] = 1
-# print("prediction before attack", pred)
+print("prediction before attack", pred)
 # print("target vector", target_tensor)
-# print("prediction after attack", pred_after_attack)
-print(torch.sum(pred_after_attack[:, TARGET_INDEX]))
+print("prediction after attack", pred_after_attack)
 print(torch.sum(pred[:, TARGET_INDEX]))
+print(torch.sum(pred_after_attack[:, TARGET_INDEX]))
 
 
--- single-label-attack.py
+++ single-label-attack.py
@@ -1,21 +1,56 @@
 import torchvision.models as models
+import torchvision.datasets.folder
 import torch
-from attack import create_adversarial_examples
+import os
+import torch.nn as nn
+import torch.optim
+import torchvision.transforms as transforms
+import glob, os
+from PIL import Image
+from pgd import create_targeted_adversarial_examples
 
-BATCH_SIZE = 1
+
+def pil_loader(path):
+	with open(path, 'rb') as f:
+		with Image.open(f) as img:
+			return img.convert('RGB')
+
+BATCH_SIZE = 1000
 TARGET_INDEX = 60
-resnet18 = models.resnet18()
-train_dataset = datasets.ImageNet('datasets/ImageNet/train/', split='train', transform=self.train_transforms, target_transform=None, download=True)
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+device = ("cuda" if torch.cuda.is_available() else "cpu")
+sigmoid = nn.Sigmoid()
 
-(images, labels) = next(iter(train_loader))
-pred = resnet18(images)
 
-target = torch.clone(pred)
-target[:, TARGET_INDEX] = 1
-adversarials = create_adversarial_examples(model, images, target, device=device)
-pred_after_attack = resnet18(adversarials)
+# Setup image net pretrained resnet18 ready for inference
+model = models.resnet18(pretrained=True).to(device)
+model.eval()
 
-print(pred)
+# preprocessing of images
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],                                std=[0.229, 0.224, 0.225])
+transform = transforms.Compose([
+	transforms.RandomResizedCrop(224),
+    transforms.ToTensor(),
+    normalize,
+])
 
+# Load batch of 1000 images
+batch = torch.empty((0, 3, 224, 224)).float()
+os.chdir("./imagenet-sample-images")
+for idx, file in enumerate(glob.glob("*.JPEG")):
+	if idx >= BATCH_SIZE:
+		break
+	img = pil_loader(file)
+	img_tensor = transform(img).unsqueeze(0)
+	batch = torch.cat((batch, img_tensor), dim=0)
+
+
+pred = model(batch)
+
+target = torch.clone(pred).detach()
+target[:, TARGET_INDEX] = 1 #target label is 60
+adversarials = create_targeted_adversarial_examples(model, batch, target, device=device)
+pred_after_attack = model(adversarials)
+
+
+print(torch.sum(sigmoid(pred[:, TARGET_INDEX]))/pred.shape[1])
+print(torch.sum(sigmoid(pred_after_attack[:, TARGET_INDEX]))/pred.shape[1])
